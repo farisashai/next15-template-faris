90d59ee9e659613490fd8b78460f0220cedcb1b3
Faris Ashai <fashai@apple.com>
2025-01-31 16:55:06 -0800

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import '@/styles/globals.scss';
+import styles from '@/styles/pages/Home.module.scss';
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1
+        style={{
+          fontWeight: 500,
+          fontSize: '2em',
+        }}
+      >
+        Hello World!
+      </h1>
+    </div>
+  );
+}
